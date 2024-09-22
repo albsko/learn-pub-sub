01e9c0102b2111197c8f66ddba45ab9ae4b7137c
Albert Skonieczny <askonieczny@fandom.com>
2024-09-22 12:03:29 +0200

    chore: add amqp support

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connUrl := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connUrl)
+	if err != nil {
+		log.Fatalf("Failed dialing %s:%+v", connUrl, err)
+	}
+	defer conn.Close()
+
+	fmt.Printf("Connected to: %s\n", connUrl)
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	sig := <-signalChan
+
+	fmt.Printf("\nReceived signal (%v). Shutting down RabbitMQ connection...\n", sig)
 }
