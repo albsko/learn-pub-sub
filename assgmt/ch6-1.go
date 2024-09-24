d6d88cfed42aba4f69c1f64b1cf6e5496b4e6941
Albert Skonieczny <askonieczny@fandom.com>
2024-09-24 15:45:20 +0200

    assgmt: add data serialization assignment

---
+++ assgmt/ch6-1.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"time"
+)
+
+func encode(gl GameLog) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(gl)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func decode(data []byte) (GameLog, error) {
+	var gl GameLog
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	err := dec.Decode(&gl)
+	if err != nil {
+		return GameLog{}, err
+	}
+	return gl, nil
+}
+
+// don't touch below this line
+
+type GameLog struct {
+	CurrentTime time.Time
+	Message     string
+	Username    string
+}
